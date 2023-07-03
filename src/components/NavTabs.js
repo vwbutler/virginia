441337103a7d08c97bd47d1ad046e36e7e45188f
Virginia Butler <vwbutler@gmail.com>
2023-07-03 12:42:11 -0400

    basic navigation

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./NavTabs.css";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
   return (
     <div>
       <h1>About Page</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        I am passionate about simplifing people's lives through technology. I
+        approach every project with a determined and courteous attitude, always
+        striving to deliver optimal results. As a Business Systems Analyst at
+        the Municipal Association of South Carolina, I recommend, implement, and
+        oversee systems that enhance the association's operations and service
+        delivery to its member cities. I am committed to continuous learning and
+        improvement to stay ahead of the curve in an ever-evolving tech
+        landscape.
       </p>
     </div>
   );
