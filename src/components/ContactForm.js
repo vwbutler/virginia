6b2e27eef781820a10569cd44aecfa6bf1d9fe5e
Virginia Butler <vwbutler@gmail.com>
2023-07-07 23:47:15 -0400

    contact form updates

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,24 +1,65 @@
 import React, { useState } from "react";
+import MyButton from "./MyButton/MyButton";
 
 function ContactForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Perform form validation if needed
+    // validation
+    let hasError = false;
+    if (!name) {
+      setNameError(true);
+      hasError = true;
+    }
+    if (!email) {
+      setEmailError(true);
+      hasError = true;
+    }
+    if (!message) {
+      setMessageError(true);
+      hasError = true;
+    }
+
+    if (hasError) {
+      return;
+    }
 
     // Call a function to handle form submission
     submitForm();
+
+    // Clear fields
+    setName("");
+    setEmail("");
+    setMessage("");
+    setNameError(false);
+    setEmailError(false);
+    setMessageError(false);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+    setNameError(false);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setEmailError(false);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+    setMessageError(false);
   };
 
   const submitForm = () => {
-    // Perform any additional processing or validation here
-
-    // Send the form data to the server using AJAX or fetch API
-    // Example using fetch API
+    //send request to server
     fetch("/api/contact", {
       method: "POST",
       headers: {
@@ -28,52 +69,86 @@ function ContactForm() {
     })
       .then((response) => response.json())
       .then((data) => {
-        // Handle the response from the server
+        // response from server
         console.log(data);
-        // Display a success message or perform any other actions
       })
       .catch((error) => {
-        // Handle any errors that occurred during form submission
         console.error(error);
-        // Display an error message or perform any other actions
       });
   };
 
+  // Inline CSS styles
+  const formStyles = {
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: "400px",
+    margin: "0 auto",
+    marginTop: "50px",
+  };
+
+  const inputStyles = {
+    marginBottom: "10px",
+    padding: "16px",
+    fontSize: "16px",
+  };
+
+  const labelStyles = {
+    marginBottom: "6px",
+    // fontWeight: "bold",
+    alignSelf: "flex-start",
+  };
+
+  const errorStyles = {
+    color: "red",
+    fontSize: "14px",
+    marginBottom: "10px",
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          name="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Submit</button>
+    <form style={formStyles} onSubmit={handleSubmit}>
+      <label htmlFor="name" style={labelStyles}>
+        Your name:
+      </label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={name}
+        onChange={handleNameChange}
+        style={inputStyles}
+      />
+      {nameError && <span style={errorStyles}>Your name is required.</span>}
+
+      <label htmlFor="email" style={labelStyles}>
+        Email address:
+      </label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={email}
+        onChange={handleEmailChange}
+        style={inputStyles}
+      />
+      {emailError && (
+        <span style={errorStyles}>Provide a valid email address.</span>
+      )}
+
+      <label htmlFor="message" style={labelStyles}>
+        Message:
+      </label>
+      <textarea
+        id="message"
+        name="message"
+        value={message}
+        onChange={handleMessageChange}
+        style={inputStyles}
+      />
+      {messageError && (
+        <span style={errorStyles}>Your message is required.</span>
+      )}
+
+      <MyButton type="submit">Send your message</MyButton>
     </form>
   );
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,7 +4,7 @@ import ContactForm from "../ContactForm";
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Virginia</h1>
+      {/* <h1>Get in touch with Virginia</h1> */}
       <ContactForm />
     </div>
   );
