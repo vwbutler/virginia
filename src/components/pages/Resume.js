2e7297326112412148c497f08051d5ced24a1434
Virginia Butler <vwbutler@gmail.com>
2023-07-07 23:54:55 -0400

    updated download resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,10 +4,11 @@ import MyButton from "../MyButton/MyButton.js";
 export default function Resume() {
   return (
     <div>
-      <p>hi</p>
       <a href="./assets/pdf/virginiabutler.pdf" download>
         {" "}
-        <MyButton class={MyButton} text="download" />{" "}
+        <MyButton class="MyButton" type="submit">
+          Download resume
+        </MyButton>
       </a>
     </div>
   );
