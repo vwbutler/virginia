f0a7c18ed146b87693c19e3d148ee61e92f77dac
Virginia Butler <vwbutler@gmail.com>
2023-07-09 12:52:15 -0400

    updated logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,7 @@ function Header() {
   return (
     <header>
       <div className="header-container">
-        <img
-          src="../assets/images/vecteezy_illustration-cartoon-rocket-cute-rocket-cartoon-vector_17745722_428.png"
-          alt="cute rocket"
-          style={{
-            maxWidth: "10%",
-            height: "10%",
-            paddingRight: "10px",
-            paddingLeft: "10px",
-            paddingTop: "10px",
-            display: "block",
-          }}
-        />
+        <div className="logo"></div>
         <div className="name">Virginia Wilkins Butler</div>
       </div>
     </header>
